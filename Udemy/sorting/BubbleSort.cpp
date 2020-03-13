00f13cee977a9db673449aa88865a2377abb5c38
Sahil <haha>
2020-03-14 00:58:32 +0530

    bubble sort made adaptive

--- Udemy/sorting/BubbleSort.cpp
+++ Udemy/sorting/BubbleSort.cpp
@@ -14,11 +14,17 @@ void swap(int *a, int *b){
 }
 
 void BubbleSort(int A[], int n){
+    int flag;
     for(int i=0; i<n-1; i++){
+        flag = 0;
         for(int j=0; j<n-1-i; j++){
             if(A[j]>A[j+1]){
                 swap(&A[j], &A[j+1]);
+                flag = 1;
             }
+        }
+        if(flag==0){
+            break;
         }
     }
 
